8a6698452914592ed9f4885c2d8c7033c3e3d79f
Tucker <tucker.chaisit@gmail.com>
2022-08-10 19:41:31 -0400

    passing event argument

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -14,16 +14,23 @@ class Counter extends Component {
   </ul>
   }
 
-  handleIncrement = () => {
-    this.setState({ count: this.state.count + 1})
-  }
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  
 
   render() { 
 
     return (
     <React.Fragment>
     <span className={this.getBadgeClasses()} >{this.formatCount()}</span>
-    <button onClick={ this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
+    <button 
+      onClick={() => this.handleIncrement()} 
+      className='btn btn-secondary btn-sm'>
+        Increment
+    </button>
     { this.renderTags() }
     </React.Fragment>
     );
