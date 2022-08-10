e7d4abf449ff1cb70173e8eb7b2da11634001d9c
Tucker <tucker.chaisit@gmail.com>
2022-08-10 19:48:53 -0400

    composing components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -24,15 +24,15 @@ class Counter extends Component {
   render() { 
 
     return (
-    <React.Fragment>
+    <div>
     <span className={this.getBadgeClasses()} >{this.formatCount()}</span>
     <button 
       onClick={() => this.handleIncrement()} 
       className='btn btn-secondary btn-sm'>
         Increment
     </button>
-    { this.renderTags() }
-    </React.Fragment>
+    {/* { this.renderTags() } */}
+    </div>
     );
   }
 
