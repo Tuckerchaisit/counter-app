bf324574d6519b1bab8687cb1d1fb8a22c0bbc4a
Tucker <tucker.chaisit@gmail.com>
2022-08-12 10:58:22 -0400

    multiple components in sync

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-    tags: ['tag1','tag2','tag3']
-  };
+  // renderTags(){
+  //   if (this.state.tags.length === 0) return <p>There are no tags!</p>;
 
-  renderTags(){
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+  //   return <ul>
+  //   { this.state.tags.map(tag=> <li key={tag}>{tag}</li>)}
+  // </ul>
+  // }
 
-    return <ul>
-    { this.state.tags.map(tag=> <li key={tag}>{tag}</li>)}
-  </ul>
-  }
-
-  handleIncrement = product => {
-    console.log(product);
-    this.setState({ value: this.state.value + 1 });
-  };
+  // handleIncrement = product => {
+  //   console.log(product);
+  //   this.setState({ value: this.state.value + 1 });
+  // };
 
   
 
@@ -28,7 +23,7 @@ class Counter extends Component {
     <h4>Counter# {this.props.id}</h4>
     <span className={this.getBadgeClasses()} >{this.formatCount()}</span>
     <button 
-      onClick={() => this.handleIncrement()} 
+      onClick={() => this.props.onIncrement(this.props.counter)} 
       className='btn btn-secondary btn-sm'>
         Increment
     </button>
@@ -40,12 +35,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += (this.state.value === 0) ? "warning" : "primary";
+    classes += (this.props.counter.value === 0) ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value: count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? 'Zero': count;
   }
 }
